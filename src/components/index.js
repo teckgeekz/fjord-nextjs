a107daad4cec349bac06389fc3691705dfc47b30
Tomas <tbdizainas@gmail.com>
2021-03-16 13:52:49 +0200

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Action from './Action';
+import ActionIcon from './ActionIcon';
+import Footer from './Footer';
+import FormField from './FormField';
+import Header from './Header';
+import HeaderAlt from './HeaderAlt';
+import HeaderImage from './HeaderImage';
+import Layout from './Layout';
+import Icon from './Icon';
+
+export {
+    Action,
+    ActionIcon,
+    Footer,
+    FormField,
+    Header,
+    HeaderAlt,
+    HeaderImage,
+    Layout,
+    Icon
+};
+
+export default {
+    Action,
+    ActionIcon,
+    Footer,
+    FormField,
+    Header,
+    HeaderAlt,
+    HeaderImage,
+    Layout,
+    Icon
+};
